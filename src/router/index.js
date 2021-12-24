76116928468c81ec9d5ce17a17da23c873d1f0f8
lxs <228389787@qq.com>
2021-12-24 09:28:09 +0800

    搭建基础框架

--- src/router/index.js
+++ src/router/index.js
@@ -4,5 +4,24 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('@/components/common/login')
+    },
+    {
+      path: '/teacher',
+      name: 'teacher',
+      component: () => import('@/components/teacher/index'),
+      children: []
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: () => import('@/components/student/index'),
+      children: []
+    }
+
+  ]
 })
